5635978eaaa3df3c16bb6c129c0929bd2382e533
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-04 13:09:02 -0400

    edit todo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import TodoList from "./components/TodoList"
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState('');
 
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo];
@@ -18,13 +19,23 @@ function App() {
   }
 
   function handleEditTodo(index) {
-
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited);
+    handleDeleteTodo(index);
   }
 
   return (
     <>
-      <TodoInput handleAddTodos={handleAddTodos} />
-      <TodoList todos={todos} handleDeleteTodo={handleDeleteTodo} />
+      <TodoInput 
+        handleAddTodos={handleAddTodos} 
+        todoValue={todoValue} 
+        setTodoValue={setTodoValue}  
+      />
+      <TodoList 
+        todos={todos} 
+        handleDeleteTodo={handleDeleteTodo} 
+        handleEditTodo={handleEditTodo}
+      />
     </>
   )
 }
