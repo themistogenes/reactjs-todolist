b5d60e3caf9f76a27facfebd6b330ab33b44db81
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-04 11:46:12 -0400

    add components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-    </>
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
   )
 }
 
