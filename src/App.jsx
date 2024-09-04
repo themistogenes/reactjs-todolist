05ed403d25fbbd23c99bce5785e466786d6cb66c
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-04 14:02:32 -0400

    persist data with local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState , useEffect } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
@@ -6,8 +6,13 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [todoValue, setTodoValue] = useState('');
 
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }));
+  }
+
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
@@ -15,6 +20,7 @@ function App() {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index;
     })
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
@@ -23,6 +29,20 @@ function App() {
     setTodoValue(valueToBeEdited);
     handleDeleteTodo(index);
   }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos');
+    if (!localTodos) {
+      return
+    }
+      
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, [])
 
   return (
     <>
